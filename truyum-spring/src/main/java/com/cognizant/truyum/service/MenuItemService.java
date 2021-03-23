e68201f0bcc5358308702aa3372a9b01a1e136c9
Uday <37234921+uday4444@users.noreply.github.com>
2021-03-23 12:21:21 +0530

    Add files via upload

---
+++ truyum-spring/src/main/java/com/cognizant/truyum/service/MenuItemService.java
@@ -0,0 +1,45 @@
+package com.cognizant.truyum.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.truyum.dao.MenuItemDao;
+import com.cognizant.truyum.model.MenuItem;
+
+@Service(value = "menuItemService")
+public class MenuItemService {
+
+	@Autowired
+	MenuItemDao menuItemDao;
+
+	public MenuItemDao getMenuItemDao() {
+		return menuItemDao;
+	}
+
+	public void setMenuItemDao(MenuItemDao menuItemDao) {
+		this.menuItemDao = menuItemDao;
+	}
+
+	public List<MenuItem> getMenuItemListAdmin() {
+		return menuItemDao.getMenuItemListAdmin();
+
+	}
+
+	public List<MenuItem> getMenuItemListCustomer() {
+		return menuItemDao.getMenuItemListCustomer();
+
+	}
+
+	public MenuItem getMenuItem(long menuItemId) {
+		return menuItemDao.getMenuItem(menuItemId);
+	}
+
+	public void editMenuItem(MenuItem menuItem) {
+
+		menuItemDao.modifyMenuItem(menuItem);
+
+	}
+
+}
---
+++ truyum-spring/src/test/java/com/cognizant/truyumspring/TruyumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruyumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
